3cfe764bb770ac6afb335134c6a7d488fe22e605
Pedro Henrique <pedropires2603@gmail.com>
2021-08-08 19:47:29 -0300

    Update: Controllers

--- src/api/app/Http/Controllers/BookingController.php
+++ src/api/app/Http/Controllers/BookingController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class login extends Controller
+class BookingController extends Controller
 {
     //
 }
--- src/api/app/Http/Controllers/LoginController.php
+++ src/api/app/Http/Controllers/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class createUser extends Controller
+class LoginController extends Controller
 {
     //
 }
